8255b59610ff931af4d5b750277ad57dcb558d23
Amit Kale <akale@webgeoservices.com>
2023-10-10 16:15:04 +0530

    1. Removed unused class and methods

--- app/src/main/java/com/webgeoservices/woosmapgeofencingexample/fragments/LocationFragment.kt
+++ app/src/main/java/com/webgeoservices/woosmapgeofencingexample/fragments/LocationFragment.kt
@@ -7,17 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ListView
 import androidx.fragment.app.Fragment
-import com.webgeoservices.woosmapgeofencingcore.database.MovingPosition
 import com.webgeoservices.woosmapgeofencingexample.R
 import com.webgeoservices.woosmapgeofencingexample.adapters.LiveLocationAdapter
-import com.webgeoservices.woosmapgeofencingexample.adapters.LocationDataAdapter
 
 
 class LocationFragment: Fragment() {
     private lateinit var locationsListView: ListView
-    private val movingPositions: ArrayList<MovingPosition> = ArrayList()
     private val liveLocations: ArrayList<Location> = ArrayList()
-    private lateinit var dataAdapter: LocationDataAdapter
     private lateinit var liveLocationDataAdapter: LiveLocationAdapter
 
     override fun onCreateView(
@@ -26,20 +22,9 @@ class LocationFragment: Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.location_list_fragment, container, false)
         locationsListView = view.findViewById(R.id.location_list)
-//        dataAdapter = LocationDataAdapter(requireContext(), movingPositions)
-//        locationsListView?.adapter = dataAdapter
         liveLocationDataAdapter = LiveLocationAdapter(requireContext(), liveLocations)
         locationsListView?.adapter = liveLocationDataAdapter
         return view
-    }
-
-    fun loadData(movingPositions: ArrayList<MovingPosition>) {
-        if (locationsListView == null) {
-            return
-        }
-        dataAdapter.clear()
-        dataAdapter.addAll(movingPositions)
-        dataAdapter.notifyDataSetChanged()
     }
 
     fun addLocation(location: Location){
@@ -49,9 +34,5 @@ class LocationFragment: Fragment() {
         liveLocationDataAdapter.insert(location, 0)
         liveLocationDataAdapter.notifyDataSetChanged()
         locationsListView.smoothScrollToPosition(0)
-    }
-
-    fun clearData() {
-        dataAdapter.clear()
     }
 }
